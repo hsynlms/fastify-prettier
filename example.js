19cb3a69679766b686bb28260d8fba49bf2ffa2f
Huseyin ELMAS <hsynlms47@gmail.com>
2020-08-28 04:14:44 +0300

    minor tweaks

--- example.js
+++ example.js
@@ -8,35 +8,37 @@ const chalk = require('chalk')
 // defaults
 const defaults = { port: 3000 }
 
-// register the plugin
-fastify.register(fastifyPrettier)
+;(async () => {
+  // register the plugin
+  await fastify.register(fastifyPrettier)
 
-// register test route
-fastify.get('/', (req, reply) => {
-  // create an object
-  const obj = {
-    blackLivesMatter: true,
-    favSinger: 'Ahmet Kaya',
-    visitedCities: ['Mardin', 'Diyarbakır', 'Rome', 'Amsterdam', 'Istanbul', 'Kotor', 'Mostar', 'Belgrade'],
-    pi: 3.14,
-    games: {
-      rdr2: 'completed',
-      gtfo: 'continues'
+  // register test route
+  fastify.get('/', (req, reply) => {
+    // create an object
+    const obj = {
+      blackLivesMatter: true,
+      favSinger: 'Ahmet Kaya',
+      visitedCities: ['Mardin', 'Diyarbakır', 'Rome', 'Amsterdam', 'Istanbul', 'Kotor', 'Mostar', 'Belgrade'],
+      pi: 3.14,
+      games: {
+        rdr2: 'completed',
+        gtfo: 'continues'
+      }
     }
-  }
 
-  // set return type
-  reply.type('application/json')
+    // set return type
+    reply.type('application/json')
 
-  // return the object
-  reply.send(obj)
-})
+    // return the object
+    reply.send(obj)
+  })
 
-// initialize the fastify server
-fastify.listen(defaults.port, () => {
-  console.log(
-    chalk.bgYellow(
-      chalk.black(`Fastify server is running on port: ${defaults.port}`)
+  // initialize the fastify server
+  fastify.listen(defaults.port, () => {
+    console.log(
+      chalk.bgYellow(
+        chalk.black(`Fastify server is running on port: ${defaults.port}`)
+      )
     )
-  )
-})
+  })
+})()
